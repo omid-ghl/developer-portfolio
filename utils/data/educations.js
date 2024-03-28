aa18f89a5866b1424b0ace2e62e944e4aaffb60d
Omid <omidgholami1@yahoo.com>
2024-03-28 19:30:13 +0330

    all

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,13 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "2016 - 2021",
+    institution: "Kharazmi university of tehran",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    title: "High School",
+    duration: "2012 - 2016",
+    institution: "Farzanegan pouya",
   },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
-  }
-]
+];
